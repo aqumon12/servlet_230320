0d27343f4b7300950f34475b1dfd72b20dd5123a
test <test@test.com>
2023-06-08 18:42:06 +0900

    20230608

--- src/main/java/com/test/lesson04/DatabaseQuiz01.java
+++ src/main/java/com/test/lesson04/DatabaseQuiz01.java
@@ -12,43 +12,45 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.test.common.MysqlService;
 
-@WebServlet("/lesson04/ex01")
-public class DatabaseEx01 extends HttpServlet{
-	
+@WebServlet("/lesson04/quiz01")
+public class DatabaseQuiz01 extends HttpServlet {
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// response의 형태
 		response.setContentType("text/plain");
-		
+
 		// DB 연결
 		MysqlService ms = MysqlService.getInstance();
-		ms.connect(); // 여기서 진짜 DB 연결!!!!!!
-		
+		ms.connect();
+
 		// DB insert
-		String insertQuery = "insert into `used_goods`"
-				+ "(`sellerId`, `title`, `description`, `price`)"
-				+ "VALUES"
-				+ "(1, '고양이 간식 팝니다', '고양이가 입맛이 까다로워서 잘 안먹어요', 2000)";
+		String insertQuery = "insert into `real_estate`"
+				+ "(`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`)" + "values"
+				+ "(3,'헤라펠리스 101동 5305호', 350, '매매', 1500000, null)";
 		try {
 			ms.update(insertQuery);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// DB select & 결과 출력
+
+		// DB 선택 & 출력
 		PrintWriter out = response.getWriter();
-		String selectQuery = "select * from `used_goods`";
+		String selectQuery = "select `address`, `area`, `type` from `real_estate`"
+				+ "order by `id` desc "
+				+ "limit 10";
 		try {
 			ResultSet res = ms.select(selectQuery);
-			while (res.next()) { // 결과 행이 있는 동안 수행
-				out.println(res.getInt("id"));
-				out.println(res.getString("title"));
-				out.println(res.getInt("price"));
-				out.println(res.getString("description"));
+			while (res.next()) {
+				String address = res.getString("address");
+				int area = res.getInt("area");
+				String type = res.getString("type");
+				
+				out.println("매물 주소: " + address + ", 면적: " + area + ", 타입: "	+ type);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		// DB 연결 해제
 		ms.disconnect();
 	}
